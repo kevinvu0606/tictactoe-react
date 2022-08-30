30172b566d393343f783b5fd3fe00fcdd94ff51c
Kevin <kevin.vu06@gmail.com>
2022-08-31 09:51:20 +1000

    feat: added game logic

--- src/components/game.js
+++ src/components/game.js
@@ -6,7 +6,54 @@ const Game = () => {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  return <div>game</div>;
+  const winner = calculateWinner(history[[stepNumber]]);
+  const xO = xIsNext ? 'X' : 'O';
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // if someones already won/if the square has already been clicked
+    if (winner || squares[i]) {
+      return;
+    }
+    squares[i] = xO;
+    //setting history of squares with spread operator and then adding new square
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    // alternating between X and O
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () => {
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : `Go to start`;
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+  };
+
+  return (
+    <>
+      <h1>React Tic Tac Toe with Hooks</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className='info-wrapper'>
+        <div>
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? 'Winner' : 'Next Player' + xO}</h3>
+      </div>
+    </>
+  );
 };
 
 export default Game;
